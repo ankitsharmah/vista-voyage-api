36e81e9e6a62b42438c7dd2935492910da14a9a5
ankitsharma06 <asrover9810@gmail.com>
2024-03-03 00:53:46 +0530

    updated url

--- src/main/java/working/hotellakewood/mapper/HotelMapper.java
+++ src/main/java/working/hotellakewood/mapper/HotelMapper.java
@@ -44,7 +44,7 @@ public class HotelMapper {
         hotelDTO.setFileName(hotel.getFileName());
 
 
-        String imageUrl = "http://localhost:9191/api/v1/hotels/hotel/" + hotel.getHotelId() + "/image";
+        String imageUrl = "https://railapp.up.railway.app/api/v1/hotels/hotel/" + hotel.getHotelId() + "/image";
         hotelDTO.setImageUrl(imageUrl);
 
         return hotelDTO;
@@ -62,7 +62,7 @@ public class HotelMapper {
         hotelDTO.setFileName(hotel.getFileName());
 
 
-        String imageUrl = "http://localhost:9191/api/v1/hotels/hotel/" + hotel.getHotelId() + "/image";
+        String imageUrl = "https://railapp.up.railway.app/api/v1/hotels/hotel/" + hotel.getHotelId() + "/image";
         hotelDTO.setImageUrl(imageUrl);
 
         return hotelDTO;
